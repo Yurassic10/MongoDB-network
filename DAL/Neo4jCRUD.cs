d529013d3d7a698d0a46de2689c4d03230eb1208
Юра <@DESKTOP-NALH133>
2024-10-23 18:47:10 +0300

    Add Neo4j task

--- DAL/Neo4jCRUD.cs
+++ DAL/Neo4jCRUD.cs
@@ -4,7 +4,7 @@ using Neo4jClient.Cypher;
 using Newtonsoft.Json;
 using System;
 using System.Linq;
-using System.Threading.Tasks; // Додано для використання Task
+using System.Threading.Tasks; 
 
 namespace DAL
 {
@@ -28,13 +28,12 @@ namespace DAL
         }
         public async Task InitializeAsync()
         {
-            await client.ConnectAsync(); // Асинхронне підключення до сервера
+            await client.ConnectAsync(); 
         }
 
-        // Асинхронний метод для підключення
         public async Task ConnectAsync()
         {
-            await client.ConnectAsync(); // Підключення до сервера
+            await client.ConnectAsync(); 
         }
 
         public async Task CreateRelationAsync(string user1Id, string user2Id)
@@ -47,65 +46,13 @@ namespace DAL
               .ExecuteWithoutResultsAsync();
         }
 
-        //private  async Task<int> PathLengthAsync(IDriver driver, string sourceNodeId, string targetNodeId)
-        //{
-        //    int pathLength = -1;
-        //    var query = @"
-        //    MATCH (start {id: $sourceNodeId}), (end {id: $targetNodeId})
-        //    MATCH p=shortestPath((start)-[*]-(end))
-        //    RETURN length(p) AS pathLength
-        //";
 
-        //    var session = driver.AsyncSession(o => o.WithDatabase("socialnetwork")); // Use the appropriate database name if not the default
-
-        //    try
-        //    {
-        //        var result = await session.ReadTransactionAsync(async tx =>
-        //        {
-        //            var resultCursor = await tx.RunAsync(query, new { sourceNodeId, targetNodeId });
-        //            var record = await resultCursor.SingleAsync();
-        //            return record["pathLength"].As<int>();
-        //        });
-
-        //        pathLength = result;
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        //Console.WriteLine($"An error occurred: {ex.Message}");
-        //    }
-        //    finally
-        //    {
-        //        await session.CloseAsync();
-        //    }
-
-        //    return pathLength;
-        //}
-
-        //public  async Task<int> GetPath(string sourceNodeId, string targetNodeId)
-        //{
-           
-        //    IDriver driver = GraphDatabase.Driver("bolt://localhost:7687", AuthTokens.Basic("neo4j", "hartovanets2204"));
-
-        //    try
-        //    {
-              
-
-        //        var pathLength = await PathLengthAsync(driver, sourceNodeId, targetNodeId);
-        //        return pathLength;
-        //    }
-        //    finally
-        //    {
-        //        await driver.CloseAsync();
-        //    }
-            
-        //}
 
         public async Task<int> PathLengthAsync(string user1Id, string user2Id)
         {
             try
             {
-                // поміняв u1 на user1 та u2 на user2
-                // було "(user1:Person {userId: $user1Id})", "(user2:Person {userId: $user2Id})", "p = shortestPath((user1)-[:Following *]->(user2))"
+               
                 var path = await client.Cypher
                     .Match("(user1:Person {userId: $user1Id})", "(user2:Person {userId: $user2Id})", "p = shortestPath((user1)-[:Following *]->(user2))")
                     .WithParam("user1Id", user1Id)
@@ -114,7 +61,7 @@ namespace DAL
                     .ResultsAsync;
 
                 return path.FirstOrDefault();
-                // Використовуйте FirstOrDefault для безпечного доступу
+                
             }
             catch (Exception)
             {
@@ -135,6 +82,3 @@ namespace DAL
 }
 
 
-//bolt://localhost:7687
-//http://localhost:7474
-// http://localhost:7474/db/data/  СТАСА
